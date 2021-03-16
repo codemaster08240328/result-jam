95ead8d4bddbfce50e2a73d5aed577f088af0cd2
Nicky Hajal <nhajal@gmail.com>
2021-03-16 13:14:06 -0500

    default avatar

--- src/components/Avatar.svelte
+++ src/components/Avatar.svelte
@@ -1,21 +1,27 @@
 <script>
   export let size = '3'
-  export let photo
-  export let user
+  export let photo = false
+  export let user = false
   let className = ''
   export {className as class}
+  $: error = false
   $: url = user ? user.photo : photo
+
+  function handlError(e) {
+    error = true
+  }
 </script>
 
 <div
-  class={`shell bg-gray rounded-full overflow-hidden transition-all duration-200 avatar flex-none ${className}`}
+  class={`shell rounded-full overflow-hidden transition-all duration-200 avatar flex-none ${className}`}
   style={`width: ${size}rem; height: ${size}rem;`}
 >
   <img
     load="lazy"
     class="w-full h-full"
+    on:error={handlError}
     src={`${
-      url
+      url && !error
         ? `${url}${
             url.includes('resultjam')
               ? `?w=${size * 16}&h=${size * 16}&crop=faces,center&fit=crop`
