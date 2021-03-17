8a625900fec093edb56ff90219af61be149c3571
Code Master <codemaster08240328@gmail.com>
2021-03-17 07:36:02 +0000

    initial commit for dev

--- src/components/Avatar.svelte
+++ src/components/Avatar.svelte
@@ -1,34 +1,24 @@
 <script>
-  export let size = '3'
-  export let photo = false
-  export let user = false
-  let className = ''
-  export {className as class}
-  $: error = false
-  $: url = user ? user.photo : photo
+  export let size = "3";
+  export let photo = false;
+  export let user = false;
+  let className = "";
+  export { className as class };
+  $: error = false;
+  $: url = user ? user.photo : photo;
 
   function handlError(e) {
-    error = true
+    error = true;
   }
 </script>
 
 <div
   class={`shell rounded-full overflow-hidden transition-all duration-200 avatar flex-none ${className}`}
-  style={`width: ${size}rem; height: ${size}rem;`}
->
+  style={`width: ${size}rem; height: ${size}rem;`}>
   <img
     load="lazy"
     class="w-full h-full"
     on:error={handlError}
-    src={`${
-      url && !error
-        ? `${url}${
-            url.includes('resultjam')
-              ? `?w=${size * 16}&h=${size * 16}&crop=faces,center&fit=crop`
-              : ''
-          }`
-        : '/images/default-avatar.png'
-    }`}
-    alt=""
-  />
+    src={`${url && !error ? `${url}${url.includes('resultjam') ? `?w=${size * 16}&h=${size * 16}&crop=faces,center&fit=crop` : ''}` : '/images/default-avatar.png'}`}
+    alt="" />
 </div>
